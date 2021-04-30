d2c3af1cfcc216ba2a8269bce0d18666ab72d13a
kai <sea.k.naka@gmail.com>
2021-04-30 15:02:15 +0900

    before flow responsive

--- js/script.js
+++ js/script.js
@@ -1,13 +1,33 @@
-$(function() {
+$(function () {
+
+  //ハンバーガーメニュー開閉
+  $('.ham').on('click', function () {
+    $('#js-black-bg').toggleClass('open');
+    $('.header-sp-nav').toggleClass('open');
+    $('.ham-line, .ham-line1, .ham-line2, .ham-line3').toggleClass('open');
+  });
 
 
+  //ハンバーガーメニューのナビリンククリック後自動で閉じる
+  $('.header-sp-menu-list a').on('click', function () {
+    $('.ham').click();
+  });
+
+  //背景とhamクリックで閉じる
+  $('.ham, .black-bg').on('click', function () {
+    if ($(this).hasClass("open")) {
+      $('.ham').click();
+    }
+  });
+
+  //faqのアコーディオン
   //最初のコンテンツ以外は非表示
   $(".faq_accordion-toggle:not(:first-of-type)").css("display", "none");
   //矢印の向きを変えておく
   $(".js-accordion-title:first-of-type").addClass("open");
 
   //タイトルがクリックされたら
-  $(".js-accordion-title").on('click', function() {
+  $(".js-accordion-title").on('click', function () {
     //クリックしたjs-accordion-title以外のずべてのopenを取る
     $(".js-accordion-title").not(this).removeClass("open");
     //クリックされたjs-accordion-title以外のfaq_accordion-toggleを閉じる
